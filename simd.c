35fc108dc2319ac2e4354dc328d75dae75781fd6
Matthew Frazer <mfrazeriguess@gmail.com>
2020-10-03 16:06:41 -0700

    simd.c

--- simd.c
+++ simd.c
@@ -5,10 +5,37 @@
 
 void print128_num(__m128 var)
 {
-    int val[4];
-	_mm_store_pd((float *)val, var);
-    printf("Numerical: %i %i %i %i \n", 
+  int val[4];
+	_mm_store_ps((float *)val, var);
+    printf("Numerical: %i %i %i %i \n",
            val[0], val[1], val[2], val[3]);
+}
+
+static int initialized;
+static __m128 ginverter;
+static int ginverter_ints[4] = {-1, -1, -1, -1};
+
+void initialize_inverter()
+{
+    ginverter = _mm_load_ps((float *)ginverter_ints);
+}
+
+void initialize_globals()
+{
+    initialize_inverter();
+    initialized = 1;
+}
+
+__m128 multiplex(__m128 a, __m128 b, __m128 selector)
+{
+    if (!initialized)
+    {
+        initialize_inverter();
+    }
+    __m128 not_selector = _mm_xor_ps(selector, ginverter);
+    __m128 a_and = _mm_and_ps(a, selector);
+    __m128 b_and = _mm_and_ps(b, not_selector);
+    return _mm_or_ps(a_and, b_and);
 }
 
 int main(void) {
@@ -19,6 +46,8 @@ int main(void) {
   int ptr4[4] = {-1, -1, -1, -1};
   int ptr5[4] = {1, 1, 1, 1};
   int ptr6[4] = {0,0,0,0};
+  printf("start");
+  initialize_globals();
 
   __m128 x = _mm_load_ps((float *) ptr);
   __m128 y = _mm_load_ps((float *) ptr2);
@@ -37,12 +66,9 @@ int main(void) {
   __m128 result1 = _mm_and_ps(x, xx3);
   print128_num(result1);
 
-__m128 result_last = _mm_xor_ps(xx3, mask);
-__m128 a_and = _mm_and_ps(result1, xx3);
-__m128 b_and = _mm_and_ps(result_last, mask);
+__m128 selected = multiplex(result1, mask, xx3);
 
-__m128 selected = _mm_or_ps(a_and,b_and);
 print128_num(selected);
-  
+
   return 0;
 }
